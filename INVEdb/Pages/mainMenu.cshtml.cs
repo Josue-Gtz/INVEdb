76b5898ea334746ce7a2c951f35128140030ba65
JosuéGtz <al23760356@ite.edu.mx>
2025-10-13 14:55:35 -0700

    Agregar archivos de proyecto.

---
+++ INVEdb/Pages/mainMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace INVEdb.Pages
+{
+    public class mainMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
